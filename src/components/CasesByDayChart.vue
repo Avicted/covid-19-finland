2768d69c4e8b58a0bbc8e6958e00d29313ad907d
victoranderssen <victor.anderssen@gmail.com>
2020-03-13 21:51:07 +0200

    Added recovered data

--- src/components/CasesByDayChart.vue
+++ src/components/CasesByDayChart.vue
@@ -4,7 +4,7 @@
     max-height="500px"
     >
     <v-card-title>
-      Reported infections by day
+      Reported infections / Recovered by day
     </v-card-title>
 
     <v-progress-circular
@@ -49,7 +49,7 @@ export default Vue.extend({
       plotOptions: {
         bar: {
           horizontal: false,
-          columnWidth: '75%',
+          columnWidth: '80%',
         },
       },
       dataLabels: {
@@ -63,11 +63,15 @@ export default Vue.extend({
     series: [{
       name: 'New infections',
       data: []
+    },
+    {
+      name: 'Recovered',
+      data: []
     }]
   }),
 
   methods: {
-    fetchChartData() {
+    fetchChartDataConfirmed() {
       const confirmedCases = store.getters['virusCasesFinland/confirmed'];
       const confirmedCasesCount = confirmedCases.length;
       const confirmedCasesByDay: string|any[] = [];
@@ -129,6 +133,70 @@ export default Vue.extend({
       }
 
       this.$data.isLoading = false;
+    },
+
+    fetchChartDataRecovered() {
+      const recoveredCases = store.getters['virusCasesFinland/recovered'];
+      const recoveredCasesCount = recoveredCases.length;
+      const recoveredCasesByDay: string|any[] = [];
+      
+      const generatedDates = [];
+      const todaysDate = new Date().toISOString();
+      let oldestDate = new Date().toISOString();
+
+      for (let i = 0; i < recoveredCasesCount; i++) {
+        const datetime = recoveredCases[i].date;
+        const date = new Date(datetime).toISOString().substr(0, 10);
+        const milliseconds = new Date(date).getTime(); 
+
+        if (Date.parse(datetime) < Date.parse(oldestDate)) {
+          oldestDate = datetime;
+        }
+
+        // Is the current date already stored? If so, increment the case count
+        const processedDatesCount = recoveredCasesByDay.length;
+        let dateAlreadyProcessed = false;
+
+        for (let i = 0; i < processedDatesCount; i++) {
+          const currentMilliseconds = recoveredCasesByDay[i][0];
+
+          if (currentMilliseconds === milliseconds) {
+            recoveredCasesByDay[i][1] = recoveredCasesByDay[i][1] + 1;
+            dateAlreadyProcessed = true;
+            break;
+          }
+        }
+
+        // If not store it
+        if (!dateAlreadyProcessed) {
+          recoveredCasesByDay.push([milliseconds, 1]);
+        }
+      }
+
+      // Generate missing dates
+      const today = moment(todaysDate);
+      const oldest = moment(oldestDate);
+
+      for (let m = moment(oldest); m.diff(today, 'days') <= 0; m.add(1, 'days')) {
+        const currentMilliseconds = new Date(m.format('YYYY-MM-DD')).getTime();
+        generatedDates.push([currentMilliseconds, 0]);
+      }
+
+      // Assign the data to the generated dates
+      for (let i = 0; i < generatedDates.length; i++) {
+        for (let j = 0; j < recoveredCasesByDay.length; j++) {
+          const currentCaseDate = recoveredCasesByDay[j][0];
+          if (currentCaseDate === generatedDates[i][0]) {
+            generatedDates[i][1] = generatedDates[i][1] + recoveredCasesByDay[j][1];
+          }
+        }
+      }
+
+      for (let i = 0; i < generatedDates.length; i++) {
+        this.$data.series[1].data.push(generatedDates[i])
+      }
+
+      this.$data.isLoading = false;
     }
   },
 
@@ -136,12 +204,16 @@ export default Vue.extend({
     this.$data.isLoading = true;
 
     if (this.$data.series[0].data === null && store.getters['virusCasesFinland/confirmed'] !== null) {
-      this.fetchChartData();
+      this.fetchChartDataConfirmed();
+    }
+    if (this.$data.series[0].data === null && store.getters['virusCasesFinland/recovered'] !== null) {
+      this.fetchChartDataRecovered();
     }
 
     this.$store.subscribe((mutation, state) => {
       if (mutation.type === 'virusCasesFinland/DATA_FETCHED') {
-        this.fetchChartData();
+        this.fetchChartDataConfirmed();
+        this.fetchChartDataRecovered();
       }
     });
   },
