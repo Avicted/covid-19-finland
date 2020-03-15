c9788f187c56e088ca2bb01e205d22ccab7afffb
victoranderssen <victor.anderssen@gmail.com>
2020-03-15 23:47:20 +0200

    New pie chart colors

--- src/components/CasesPerDistrictChart.vue
+++ src/components/CasesPerDistrictChart.vue
@@ -40,7 +40,25 @@ export default Vue.extend({
         mode: 'dark',
         palette: 'palette7' // upto palette10
       },
-      // colors: ['#ec407a', '#4caf50'],
+      colors: [
+        "#e57373", 
+        "#f06292",
+        "#ba68c8",
+        "#9575cd",
+        "#7986cb",
+        "#64b5f6",
+        "#4fc3f7",
+        "#4dd0e1",
+        "#4db6ac",
+        "#81c784",
+        "#aed581",
+        "#dce775",
+        "#fff176",
+        "#ffd54f",
+        "#ffb74d",
+        "#ff8a65",
+        "#a1887f"
+      ],
       chart: {
         stacked: false,
         type: 'donut',
