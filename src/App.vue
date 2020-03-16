981549fab65f97a1bc840aaa5bf04042edfc3972
victoranderssen <victor.anderssen@gmail.com>
2020-03-16 13:59:59 +0200

    Updated footer with data source

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
       
     </v-app-bar> -->
 
-    <v-container style="margin-bottom: 75px;">
+    <v-container style="margin-bottom: 120px;">
       <v-row>
         <v-col cols="12">
-          <p class="display-1 mb-0" style="font-family: monospace !important;">Finland COVID-19 data</p>
+          <p class="display-1 mb-0" style="font-family: monospace !important;">
+            Finland COVID-19 data
+          </p>
         </v-col>
       </v-row>
 
@@ -16,12 +18,23 @@
       </v-content>
     </v-container>
 
-    <v-footer absolute class="font-weight-light" style="background: transparent; font-family: monospace;">
-      <v-col
-        class="text-center"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} - <strong><a href="https://github.com/Avicted" class="pink--text" style="text-decoration: none;">Avicted</a></strong>
+    <v-footer
+      absolute
+      class="font-weight-light"
+      style="background: transparent; font-family: monospace;"
+    >
+      <v-col class="text-center" cols="12">
+        <p class="mb-0">
+          <a href="https://github.com/Avicted/covid-19-finland">Source code</a>
+        </p>
+        <p class="mb-0">
+          <a href="https://github.com/HS-Datadesk/koronavirus-avoindata">Data source</a>
+          © 2020 Helsingin Sanomat
+        </p>
+        <p class="mb-0">
+          Developed by:
+          <a href="https://twitter.com/Victoranderssen">Victor Anderssén</a>
+        </p>
       </v-col>
     </v-footer>
   </v-app>
@@ -35,7 +48,7 @@ export default Vue.extend({
   name: "App",
 
   mounted() {
-    store.dispatch('virusCasesFinland/fetchData');
+    store.dispatch("virusCasesFinland/fetchData");
   }
 });
 </script>
